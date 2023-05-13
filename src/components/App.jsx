3dd35a74cbcf732e3823a595b09ee8e2807a92f5
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-13 12:07:42 +0300

    add logic for saving contacts in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,31 @@ import ContactList from './ContactList/ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  componentDidMount() {
+    const parsedContactsFromLocalStorage = JSON.parse(
+      localStorage.getItem('contacts')
+    );
+    // console.log(parsedContactsFromLocalStorage);
+    if (
+      parsedContactsFromLocalStorage &&
+      parsedContactsFromLocalStorage.length > 0
+    ) {
+      this.setState({ contacts: parsedContactsFromLocalStorage });
+    }
+  }
+
+  componentDidUpdate(_, { contacts: prevContacts }) {
+    // console.log(contacts);
+    // console.log(this.state.contacts);
+    const { contacts: newContacts } = this.state;
+    if (prevContacts !== newContacts) {
+      this.setState({ contacts: newContacts });
+      localStorage.setItem('contacts', JSON.stringify(newContacts));
+    }
+  }
 
   addNewContact = ({ name, number }) => {
     if (this.checkContactExist(name)) {
